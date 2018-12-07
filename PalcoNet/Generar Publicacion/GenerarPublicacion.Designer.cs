3b30b23fdb98c2dd9929e2f1c99209ea081b8b66
Alan Vaudagna <alanvaudagna@gmail.com>
2018-12-07 20:28:29 -0300

    GenerarPublicacion design.

---
+++ PalcoNet/Generar Publicacion/GenerarPublicacion.Designer.cs
@@ -0,0 +1,401 @@
+﻿namespace PalcoNet.GenerarPublicacion
+{
+    partial class GenerarPublicacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_nombre_rol = new System.Windows.Forms.Label();
+            this.tx_nombre_rol = new System.Windows.Forms.TextBox();
+            this.group_alta_rol = new System.Windows.Forms.GroupBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.txtDepto = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txNumero = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txPiso = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txCp = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txLocalidad = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txCalle = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_alta_rol = new System.Windows.Forms.Button();
+            this.btn_volver = new System.Windows.Forms.Button();
+            this.group_alta_rol.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_nombre_rol
+            // 
+            this.lbl_nombre_rol.AutoSize = true;
+            this.lbl_nombre_rol.Location = new System.Drawing.Point(22, 33);
+            this.lbl_nombre_rol.Name = "lbl_nombre_rol";
+            this.lbl_nombre_rol.Size = new System.Drawing.Size(63, 13);
+            this.lbl_nombre_rol.TabIndex = 0;
+            this.lbl_nombre_rol.Text = "Descripcion";
+            // 
+            // tx_nombre_rol
+            // 
+            this.tx_nombre_rol.Location = new System.Drawing.Point(93, 33);
+            this.tx_nombre_rol.Name = "tx_nombre_rol";
+            this.tx_nombre_rol.Size = new System.Drawing.Size(400, 20);
+            this.tx_nombre_rol.TabIndex = 1;
+            // 
+            // group_alta_rol
+            // 
+            this.group_alta_rol.BackColor = System.Drawing.Color.White;
+            this.group_alta_rol.Controls.Add(this.label13);
+            this.group_alta_rol.Controls.Add(this.comboBox3);
+            this.group_alta_rol.Controls.Add(this.textBox3);
+            this.group_alta_rol.Controls.Add(this.label12);
+            this.group_alta_rol.Controls.Add(this.label5);
+            this.group_alta_rol.Controls.Add(this.comboBox2);
+            this.group_alta_rol.Controls.Add(this.txtDepto);
+            this.group_alta_rol.Controls.Add(this.label11);
+            this.group_alta_rol.Controls.Add(this.txNumero);
+            this.group_alta_rol.Controls.Add(this.label10);
+            this.group_alta_rol.Controls.Add(this.txPiso);
+            this.group_alta_rol.Controls.Add(this.label9);
+            this.group_alta_rol.Controls.Add(this.txCp);
+            this.group_alta_rol.Controls.Add(this.label7);
+            this.group_alta_rol.Controls.Add(this.label8);
+            this.group_alta_rol.Controls.Add(this.txLocalidad);
+            this.group_alta_rol.Controls.Add(this.label6);
+            this.group_alta_rol.Controls.Add(this.txCalle);
+            this.group_alta_rol.Controls.Add(this.label4);
+            this.group_alta_rol.Controls.Add(this.comboBox1);
+            this.group_alta_rol.Controls.Add(this.label3);
+            this.group_alta_rol.Controls.Add(this.dateTimePicker1);
+            this.group_alta_rol.Controls.Add(this.textBox2);
+            this.group_alta_rol.Controls.Add(this.label2);
+            this.group_alta_rol.Controls.Add(this.textBox1);
+            this.group_alta_rol.Controls.Add(this.label1);
+            this.group_alta_rol.Controls.Add(this.btn_alta_rol);
+            this.group_alta_rol.Controls.Add(this.tx_nombre_rol);
+            this.group_alta_rol.Controls.Add(this.lbl_nombre_rol);
+            this.group_alta_rol.Location = new System.Drawing.Point(36, 76);
+            this.group_alta_rol.Name = "group_alta_rol";
+            this.group_alta_rol.Size = new System.Drawing.Size(538, 364);
+            this.group_alta_rol.TabIndex = 3;
+            this.group_alta_rol.TabStop = false;
+            this.group_alta_rol.Text = "Datos de Publicacion";
+            this.group_alta_rol.Enter += new System.EventHandler(this.group_alta_rol_Enter);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(243, 229);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(40, 13);
+            this.label13.TabIndex = 57;
+            this.label13.Text = "Estado";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(294, 226);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 56;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(167, 270);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 20);
+            this.textBox3.TabIndex = 55;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(29, 273);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(132, 13);
+            this.label12.TabIndex = 54;
+            this.label12.Text = "Username del responsable";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 53;
+            this.label5.Text = "Grado";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(88, 226);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 52;
+            // 
+            // txtDepto
+            // 
+            this.txtDepto.Location = new System.Drawing.Point(490, 180);
+            this.txtDepto.Name = "txtDepto";
+            this.txtDepto.Size = new System.Drawing.Size(27, 20);
+            this.txtDepto.TabIndex = 51;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(289, 183);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(44, 13);
+            this.label11.TabIndex = 50;
+            this.label11.Text = "Número";
+            // 
+            // txNumero
+            // 
+            this.txNumero.Location = new System.Drawing.Point(339, 180);
+            this.txNumero.Name = "txNumero";
+            this.txNumero.Size = new System.Drawing.Size(43, 20);
+            this.txNumero.TabIndex = 42;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(388, 183);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(27, 13);
+            this.label10.TabIndex = 49;
+            this.label10.Text = "Piso";
+            // 
+            // txPiso
+            // 
+            this.txPiso.Location = new System.Drawing.Point(421, 180);
+            this.txPiso.Name = "txPiso";
+            this.txPiso.Size = new System.Drawing.Size(27, 20);
+            this.txPiso.TabIndex = 43;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(454, 183);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(30, 13);
+            this.label9.TabIndex = 48;
+            this.label9.Text = "Dpto";
+            // 
+            // txCp
+            // 
+            this.txCp.Location = new System.Drawing.Point(416, 144);
+            this.txCp.Name = "txCp";
+            this.txCp.Size = new System.Drawing.Size(96, 20);
+            this.txCp.TabIndex = 40;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(388, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 13);
+            this.label7.TabIndex = 47;
+            this.label7.Text = "CP";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(29, 147);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 46;
+            this.label8.Text = "Localidad";
+            // 
+            // txLocalidad
+            // 
+            this.txLocalidad.Location = new System.Drawing.Point(88, 144);
+            this.txLocalidad.Name = "txLocalidad";
+            this.txLocalidad.Size = new System.Drawing.Size(281, 20);
+            this.txLocalidad.TabIndex = 39;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 45;
+            this.label6.Text = "Calle";
+            // 
+            // txCalle
+            // 
+            this.txCalle.Location = new System.Drawing.Point(88, 180);
+            this.txCalle.Name = "txCalle";
+            this.txCalle.Size = new System.Drawing.Size(195, 20);
+            this.txCalle.TabIndex = 41;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(357, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Rubro";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(399, 98);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Fecha y hora del espectaculo";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(182, 99);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(151, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(265, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(36, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(167, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cantidad Asientos";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(36, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Cantidad Filas";
+            // 
+            // btn_alta_rol
+            // 
+            this.btn_alta_rol.Location = new System.Drawing.Point(239, 310);
+            this.btn_alta_rol.Name = "btn_alta_rol";
+            this.btn_alta_rol.Size = new System.Drawing.Size(94, 39);
+            this.btn_alta_rol.TabIndex = 0;
+            this.btn_alta_rol.Text = "Generar";
+            this.btn_alta_rol.UseVisualStyleBackColor = true;
+            this.btn_alta_rol.Click += new System.EventHandler(this.btn_alta_rol_Click);
+            // 
+            // btn_volver
+            // 
+            this.btn_volver.Location = new System.Drawing.Point(61, 456);
+            this.btn_volver.Name = "btn_volver";
+            this.btn_volver.Size = new System.Drawing.Size(83, 44);
+            this.btn_volver.TabIndex = 4;
+            this.btn_volver.Text = "Volver";
+            this.btn_volver.UseVisualStyleBackColor = true;
+            this.btn_volver.Click += new System.EventHandler(this.btn_volver_Click);
+            // 
+            // GenerarPublicacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 512);
+            this.Controls.Add(this.btn_volver);
+            this.Controls.Add(this.group_alta_rol);
+            this.Name = "GenerarPublicacion";
+            this.Text = "Generar Publicacion";
+            this.group_alta_rol.ResumeLayout(false);
+            this.group_alta_rol.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_nombre_rol;
+        private System.Windows.Forms.TextBox tx_nombre_rol;
+        private System.Windows.Forms.GroupBox group_alta_rol;
+        private System.Windows.Forms.Button btn_alta_rol;
+        private System.Windows.Forms.Button btn_volver;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox txtDepto;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txNumero;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txPiso;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txCp;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txLocalidad;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txCalle;
+    }
+}
---
+++ PalcoNet/Generar Publicacion/GenerarPublicacion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PalcoNet.Modelo;
+using MaterialSkin.Controls;
+using MaterialSkin;
+using PalcoNet.Repositorios;
+
+namespace PalcoNet.GenerarPublicacion
+{
+    public partial class GenerarPublicacion : MaterialForm
+    {
+
+        public GenerarPublicacion()
+        {
+            InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+           
+        }
+
+        private void btn_volver_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btn_alta_rol_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void limpiarVentana()
+        {
+           
+
+        }
+
+        private void listarFuncionalidades()
+        {
+           
+        }
+
+        private void group_alta_rol_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
