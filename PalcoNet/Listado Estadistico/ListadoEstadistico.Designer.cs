d5182eaf3e2845b2b13cd62169c7966823ac0a11
daro <dfabricio@qualyt.com>
2018-12-18 20:29:51 -0300

    lsitado clientes en proceso

--- PalcoNet/Listado Estadistico/ListadoEstadistico.Designer.cs
+++ PalcoNet/Listado Estadistico/ListadoEstadistico.Designer.cs
@@ -1,4 +1,8 @@
-﻿namespace PalcoNet.Listado_Estadistico
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+namespace PalcoNet.Listado_Estadistico
 {
     partial class ListadoEstadistico
     {
@@ -6,6 +10,7 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        
 
         /// <summary>
         /// Clean up any resources being used.
@@ -28,11 +33,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.tipoListado = new System.Windows.Forms.ComboBox();
+            this.data_Listado = new System.Windows.Forms.DataGridView();
+            this.trimestre = new System.Windows.Forms.TextBox();
+            this.btn_buscar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.data_Listado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tipoListado
+            // 
+            this.tipoListado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tipoListado.FormattingEnabled = true;
+            this.tipoListado.Location = new System.Drawing.Point(12, 81);
+            this.tipoListado.Name = "tipoListado";
+            this.tipoListado.Size = new System.Drawing.Size(137, 21);
+            this.tipoListado.TabIndex = 6;
+            this.tipoListado.SelectedIndexChanged += new System.EventHandler(this.tipoListado_SelectedIndexChanged);
+            // 
+            // data_Listado
+            // 
+            this.data_Listado.AllowUserToAddRows = false;
+            this.data_Listado.AllowUserToDeleteRows = false;
+            this.data_Listado.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.data_Listado.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.data_Listado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.NullValue = "-";
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.data_Listado.DefaultCellStyle = dataGridViewCellStyle3;
+            this.data_Listado.Location = new System.Drawing.Point(12, 131);
+            this.data_Listado.Name = "data_Listado";
+            this.data_Listado.ReadOnly = true;
+            this.data_Listado.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.data_Listado.Size = new System.Drawing.Size(627, 237);
+            this.data_Listado.TabIndex = 24;
+            this.data_Listado.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.data_Listado_CellContentClick);
+            // 
+            // trimestre
+            // 
+            this.trimestre.AccessibleDescription = "";
+            this.trimestre.Location = new System.Drawing.Point(202, 82);
+            this.trimestre.Margin = new System.Windows.Forms.Padding(2);
+            this.trimestre.Name = "trimestre";
+            this.trimestre.Size = new System.Drawing.Size(53, 20);
+            this.trimestre.TabIndex = 25;
+            this.trimestre.TextChanged += new System.EventHandler(this.trimestre_TextChanged);
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_buscar.Location = new System.Drawing.Point(429, 68);
+            this.btn_buscar.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_buscar.Name = "btn_buscar";
+            this.btn_buscar.Size = new System.Drawing.Size(91, 47);
+            this.btn_buscar.TabIndex = 26;
+            this.btn_buscar.Text = "Buscar";
+            this.btn_buscar.UseVisualStyleBackColor = true;
+            this.btn_buscar.Click += new System.EventHandler(this.btn_buscar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(286, 82);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(53, 20);
+            this.textBox1.TabIndex = 27;
+            // 
+            // ListadoEstadistico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(683, 403);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_buscar);
+            this.Controls.Add(this.trimestre);
+            this.Controls.Add(this.data_Listado);
+            this.Controls.Add(this.tipoListado);
+            this.Name = "ListadoEstadistico";
+            this.Text = "Listado estadistico";
+            this.Load += new System.EventHandler(this.ListadoEstadistico_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.data_Listado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
+        private void trimestre_TextChanged(object sender, System.EventArgs e)
+        {
+        }
+
+        
+
+        private void data_Listado_CellContentClick(object sender, System.Windows.Forms.DataGridViewCellEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void tipoListado_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            
+            
+            
+        }
         #endregion
+
+        private System.Windows.Forms.ComboBox tipoListado;
+        private System.Windows.Forms.TextBox trimestre;
+        private System.Windows.Forms.DataGridView data_Listado;
+        private System.Windows.Forms.Button btn_buscar;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- PalcoNet/Listado Estadistico/ListadoEstadistico.cs
+++ PalcoNet/Listado Estadistico/ListadoEstadistico.cs
@@ -1,9 +1,11 @@
 ﻿using MaterialSkin;
 using MaterialSkin.Controls;
+using PalcoNet.Repositorios;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,9 +15,12 @@ using System.Windows.Forms;
 namespace PalcoNet.Listado_Estadistico
 {
     public partial class ListadoEstadistico : MaterialForm
-
     {
-        
+        private List<SqlParameter> parametros { get; set; }
+        private string tipoListadoseleccionado{get;set;} 
+        DataTable tablaEmpresasConLocalidades = new DataTable();
+        DataTable tablaClientesConMasPuntosVencidos= new DataTable();
+        DataTable tabla_clientesConMasCompras = new DataTable();
         public ListadoEstadistico()
         {
             InitializeComponent();
@@ -23,22 +28,14 @@ namespace PalcoNet.Listado_Estadistico
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
             materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
-
-        }
+            var parametros = new List<SqlParameter>();
+            tipoListado.Items.Add("EmpresasConMasLocalidadesSinVender");
+            tipoListado.Items.Add("ClientesConMasPuntosVencidos");
+            tipoListado.Items.Add("ClientesConMasComprasRealizadas");
+                    }
         private void initColumnsParaEmpresasConLocalidades()
-        {
-            tabla_clientes.Columns.Add("Tipo doc", typeof(string));
-            tabla_clientes.Columns.Add("Documento", typeof(string));
-            tabla_clientes.Columns.Add("Cuil", typeof(string));
-            tabla_clientes.Columns.Add("Nombre", typeof(string));
-            tabla_clientes.Columns.Add("Apellido", typeof(string));
-            tabla_clientes.Columns.Add("Email", typeof(string));
-            tabla_clientes.Columns.Add("Calle", typeof(string));
-            tabla_clientes.Columns.Add("Numero", typeof(string));
-            tabla_clientes.Columns.Add("Localidad", typeof(string));
-            tabla_clientes.Columns.Add("Codigo postal", typeof(string));
-            tabla_clientes.Columns.Add("Habilitado", typeof(string));
-            actualizarTabla();
+        {   data_Listado.Columns.Add(,);
+            
         }
         private void initColumnsParaClientesConMasPuntosVencidos()
         {
@@ -69,6 +66,61 @@ namespace PalcoNet.Listado_Estadistico
             tabla_clientes.Columns.Add("Codigo postal", typeof(string));
             tabla_clientes.Columns.Add("Habilitado", typeof(string));
             actualizarTabla();
+        }
+
+        private void data_clientes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void combo_roles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tipoListadoseleccionado =(string)tipoListado.SelectedItem;
+            
+        }
+
+        private void tx_dni_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (int.Parse(trimestre.Text) > 4 || int.Parse(trimestre.Text) < 1)
+                    throw new Exception("ingrese un valor de trimestre valido");
+                else { parametros.Add(new SqlParameter("@Trimestre", int.Parse(trimestre.Text))); }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Advertencia.", MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void ListadoEstadistico_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {SqlDataReader reader=new SqlDataReader();
+            switch (tipoListadoseleccionado)
+            {   
+                case ("EmpresasConMasLocalidadesSinVender"): 
+                    
+                   reader =  DataBase.ejecutarFuncion("", parametros).ExecuteReader();
+                   while (reader.Read())
+                   { }   
+                    break;
+                case ("ClientesConMasPuntosVencidos"): 
+                    reader =  DataBase.ejecutarFuncion("", parametros).ExecuteReader();
+                   while (reader.Read())
+                   { } 
+                    break;
+                case ("ClientesConMasComprasRealizadas"):
+                    reader =  DataBase.ejecutarFuncion("", parametros).ExecuteReader();
+                   while (reader.Read())
+                   { } 
+                    break;
+                default: break;
+            }
         }
     }
 }
